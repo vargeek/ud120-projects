c19220f17ae7b9c4815ef7364bfcc5d5b319fc04
vargeek <qbzhuce@hotmail.com>
2017-01-16 15:27:49 +0800

    ex3-决策树练习

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,6 +24,40 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+# no. of Chris training emails: 7936
+# no. of Sara training emails: 7884
+
+# print('dimension of features: {}'.format(len(features_train[0])))
+# dimension of features: 3785
+# selector = SelectPercentile(f_classif, percentile=1)
+# dimension of features: 379
+
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+begin_time = time()
+clf.fit(features_train, labels_train)
+end_time = time()
+print("training time:{} s.".format(round(end_time - begin_time, 3)))
+
+begin_time = time()
+pred = clf.predict(features_test)
+end_time = time()
+print("predicting time:{} s.".format(round(end_time - begin_time, 3)))
+
+accuracy = accuracy_score(pred, labels_test)
+print('The test accuracy is: {}'.format(accuracy))
+
+# percentile=10
+# training time:62.406 s.
+# predicting time:0.08 s.
+# The test accuracy is: 0.978953356086
+
+# percentile=1
+# training time:5.358 s.
+# predicting time:0.005 s.
+# The test accuracy is: 0.966439135381
 
 
 #########################################################
