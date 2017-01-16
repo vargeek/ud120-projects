a766c807ddfbd970b4fbdaad6d3bafd1513a98ac
vargeek <qbzhuce@hotmail.com>
2017-01-16 20:20:29 +0800

    ex4-自己选择学习算法

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,10 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from time import time
+from sklearn.ensemble import AdaBoostClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -35,10 +39,20 @@ plt.show()
 
 
 
+accuracy_max = -10^8
+for n in [1,3,10,30,100,300,1000,3000]:
+    clf_temp = clf_temp = RandomForestClassifier(n_estimators=n)
+    clf_temp.fit(features_train, labels_train)
+    pred = clf_temp.predict(features_test)
+    accuracy = accuracy_score(pred, labels_test)
+    print('The test accuracy of {} is: {}'.format(n, accuracy))
+    if accuracy > accuracy_max:
+        accuracy_max = accuracy
+        clf = clf_temp
 
 
 
 try:
-    prettyPicture(clf, features_test, labels_test)
+    prettyPicture(clf_temp, features_test, labels_test)
 except NameError:
     pass
