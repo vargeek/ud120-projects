4fb25db3b1e59b732507a56c3fe41f09e6174fd2
vargeek <qbzhuce@hotmail.com>
2017-01-17 15:04:42 +0800

    ex5-回归

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+# features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,11 +39,21 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+slope = reg.coef_[0]
+intercept = reg.intercept_
+score_train = reg.score(feature_train, target_train)
+score_test = reg.score(feature_test, target_test)
+print('slope: {}, intercept: {}'.format(slope, intercept))
+print('score_train: {}'.format(score_train))
+print('score_test: {}'.format(score_test))
+# score_train: 0.0455091926995
+# score_test: -1.48499241737
+# => features_list = ["bonus", "long_term_incentive"]
+# score_train: 0.217085971258
+# score_test: -0.59271289995
 
 
 ### draw the scatterplot, with color-coded training and testing points
@@ -64,6 +75,10 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+reg.fit(feature_test, target_test)
+slope_test = reg.coef_[0]
+print('slope_test: {}'.format(slope_test))
+plt.plot(feature_train, reg.predict(feature_train), color="b")
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
